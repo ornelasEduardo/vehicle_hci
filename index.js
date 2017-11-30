3f5061f100fa1f5ff1c5bca8e260fd0aaa015d0a
Eduardo Ornelas <eddie@Eduardos-MacBook-Pro.local>
2017-11-29 23:51:23 -0700

    Fixed hammerjs solution so that swipes up and down are properly read. The left and right swipe now cycle through the features carousel style. Added a background color on the icons to show where in the process the user is. Also, fixed the issue of gmap not showing up on page load. Footer images now have widths of 55px and heights of 45px

--- index.js
+++ index.js
@@ -1,204 +1,165 @@
-var stage = document.getElementById('stage');
-$stage = jQuery(stage);
-
 var features = ["map-container", "music-container"]
 var requestedFeature = "map-container";
 
-$(document).ready(function (){
-  // create a manager for that element
-  var manager = new Hammer.Manager(stage);
+$(function () { 
+  var element = document.getElementById("stage"); 
+  var mc = new Hammer(document.body);
 
-  var Pan = new Hammer.Pan({
-      pointers: 2,
-  });
-  var Swipe = new Hammer.Swipe({
-      pointers: 2,
-  });
+  featureIndex = features.indexOf(requestedFeature);
+  previousFeature = requestedFeature;
 
-  Swipe.recognizeWith([Pan])
-
-  manager.add(Pan);
-  manager.add(Swipe)
-
-  manager.on('swipe', function (e){
-    featureIndex = features.indexOf(requestedFeature);
-    previousFeature = requestedFeature;
-
-    //swipe left
-    if(e.direction === 2){
-      if(featureIndex != (features.length - 1))
-        requestedFeature = features[features.indexOf(requestedFeature) + 1];
-    }
-    //swipe right 
-    else if(e.direction === 4){
-      if(features.indexOf(requestedFeature) != 0)
-        requestedFeature = features[features.indexOf(requestedFeature) - 1];
-    }
-
-    changeViewTo(requestedFeature, previousFeature);
+  mc.get('swipe').set({ 
+    "direction": Hammer.DIRECTION_ALL,
+    "pointers": 2,
+    "threshold": 3,
+    "velocity": .1,
   });
 
-  function changeViewTo(requestedFeature, previousFeature) {
-    previousFeatureDomElem = $('.' + previousFeature);
-    requestedFeatureDomElem = $('.' + requestedFeature);
+  mc.get("pan").set({
+    "pointers": 2,
+    "threshold": 3, 
+    "velocity": .1,
+  });
 
-    previousFeatureDomElem.addClass("d-none");
-    requestedFeatureDomElem.removeClass("d-none");
-  }
+  mc.on("swipeleft", function (e) { 
+    // if(featureIndex != (features.length - 1))
+    //   requestedFeature = features[features.indexOf(requestedFeature) + 1];
+    var temp = features.shift();
+    features.push(temp);
+    
+    changeViewTo(features[0], features[features.length - 1]);
+  }); 
+      
+  mc.on("swiperight", function (e) { 
+    // if(features.indexOf(requestedFeature) != 0)
+    //   requestedFeature = features[features.indexOf(requestedFeature) - 1];
+    
+      var temp = features.pop();
+      features.unshift(temp);
+      
+      changeViewTo(features[0], features[1]);
+  });
+  
+  mc.on("panup", function (e) { 
+    var inputWidth = parseInt($('#volume-control').css("width").replace("px", ""));
+    var moveBy = Math.abs((e.deltaY / 100));
+    console.log("Up: " + moveBy);
+    $('#volume-control').removeClass("d-none");
+    $('#volume-control').val(parseInt($('#volume-control').val()) + moveBy);
+  }); 
+  
+  mc.on("pandown", function (e) { 
+    var inputWidth = parseInt($('#volume-control').css("width").replace("px", ""));
+    var moveBy = Math.abs((e.deltaY / 100));
+    //moveBy = moveBy < 0 ? -1 * moveBy : moveBy;
+    console.log("Down: " + moveBy);
+    $('#volume-control').val(parseInt($('#volume-control').val()) - moveBy);
+  });
+
+  mc.on("panend", function (e){
+    $('#volume-control').addClass("d-none"); 
+  });
 
   function initMap() {
-      var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 18,
-          center: {lat: 32.2319, lng: -110.9501},
-          styles: [
-              {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-              {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-              {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-              {
-                featureType: 'administrative.locality',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-              },
-              {
-                featureType: 'poi',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-              },
-              {
-                featureType: 'poi.park',
-                elementType: 'geometry',
-                stylers: [{color: '#263c3f'}]
-              },
-              {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#6b9a76'}]
-              },
-              {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [{color: '#38414e'}]
-              },
-              {
-                featureType: 'road',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#212a37'}]
-              },
-              {
-                featureType: 'road',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#9ca5b3'}]
-              },
-              {
-                featureType: 'road.highway',
-                elementType: 'geometry',
-                stylers: [{color: '#746855'}]
-              },
-              {
-                featureType: 'road.highway',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#1f2835'}]
-              },
-              {
-                featureType: 'road.highway',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#f3d19c'}]
-              },
-              {
-                featureType: 'transit',
-                elementType: 'geometry',
-                stylers: [{color: '#2f3948'}]
-              },
-              {
-                featureType: 'transit.station',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-              },
-              {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [{color: '#17263c'}]
-              },
-              {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#515c6d'}]
-              },
-              {
-                featureType: 'water',
-                elementType: 'labels.text.stroke',
-                stylers: [{color: '#17263c'}]
-              }
-            ],
-            disableDefaultUI: true,
-      });
-  }
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: {lat: 32.2319, lng: -110.9501},
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ],
+          disableDefaultUI: true,
+    });
+}
 
-  initMap();
+initMap();
 
-  var knob = $('.knob');
-  var angle = 0;
-  var minangle = 0;
-  var maxangle = 270;
-  
-  function moveKnob(direction) {
-  
-      if(direction == 'up') {
-          if((angle + 2) <= maxangle) {
-              angle = angle + 2;
-              setAngle();
-          }
-      }
-  
-      else if(direction == 'down') {
-          if((angle - 2) >= minangle) {
-              angle = angle - 2;
-              setAngle();
-          }
-      }
-  
-  }
-  
-  function setAngle() {
-  
-      // rotate knob
-      knob.css({
-          '-moz-transform':'rotate('+angle+'deg)',
-          '-webkit-transform':'rotate('+angle+'deg)',
-          '-o-transform':'rotate('+angle+'deg)',
-          '-ms-transform':'rotate('+angle+'deg)',
-          'transform':'rotate('+angle+'deg)'
-      });
-  
-      // highlight ticks
-      var activeTicks = (Math.round(angle / 10) + 1);
-      $('.tick').removeClass('activetick');
-      $('.tick').slice(0,activeTicks).addClass('activetick');
-  
-      // update % value in text
-      var pc = Math.round((angle/270)*100);
-      $('.current-value').text(pc+'%');
-  
-  }
-  
-  // mousewheel event - firefox
-  knob.bind('DOMMouseScroll', function(e){
-      if(e.originalEvent.detail > 0) {
-          moveKnob('down');
-      } else {
-          moveKnob('up');
-      }
-      return false;
-  });
-  
-  // mousewheel event - ie, safari, opera
-  knob.bind('mousewheel', function(e){
-      if(e.originalEvent.wheelDelta < 0) {
-          moveKnob('down');
-      } else {
-          moveKnob('up');
-      }
-      return false;
-  });
-  
 });
+
+$('.footer-image-container').on('click', function (){
+  $('.active').removeClass('active');
+  $(this).parents('.col-md-1').addClass('active');
+});
+
+function changeViewTo(requestedFeature, previousFeature) {
+  previousFeatureDomElem = $('.' + previousFeature);
+  requestedFeatureDomElem = $('.' + requestedFeature);
+  previousFeatureDomElem.addClass("d-none");
+  requestedFeatureDomElem.removeClass("d-none");
+}
